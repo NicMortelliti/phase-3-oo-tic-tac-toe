0f7664c4ca4590991d5fba027bfbf02f2e289014
NicMortelliti <npmortelliti@gmail.com>
2022-06-04 13:07:20 -0700

    Assignent complete

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,50 +1,40 @@
 class TicTacToe
-  attr_accessor :board
-
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
-
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
 
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
+
   def display_board
-    puts "------------"
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(string)
-    string.to_i - 1
-  end 
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
 
-  def move(index, token = "X")
+  def move(index, token)
     @board[index] = token
   end
 
-  def position_taken?(position)
-    if board[position] == " "
-      false
-    else board[position] == "X" || "O"
-      true 
-    end
+  def position_taken?(index)
+    @board[index] != " "
   end
 
-  def valid_move?(position)
-    !position_taken?(position) && position.between?(0,8)
+  def valid_move?(index)
+    !position_taken?(index) && index.between?(0,8)
   end
 
   def turn_count
-    @board.filter{|position| position != " "}.count
+    @board.count{|square| square != " " }
   end
 
   def current_player
@@ -52,22 +42,46 @@ class TicTacToe
   end
 
   def turn
-    input = gets.chomp
-    position = input_to_index(input)
-    if valid_move?(position)
+    puts "Please enter a number (1-9):"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
       token = current_player
-      move(position, token)
-      display_board
+      move(index, token)
     else
       turn
     end
+    display_board
   end
 
   def won?
-    WIN_COMBINATIONS.any? do |indexes|
-      if board[indexes[0]] == board[indexes[1]] && board[indexes[0]] == board[indexes[2]]
-        return indexes
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
       end
     end
+  end
+
+  def full?
+    @board.all?{|square| square != " " }
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if combo = won?
+      @board[combo[0]]
+    end
+  end
+
+  def play
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
   end
 end
